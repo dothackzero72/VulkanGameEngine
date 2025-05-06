24116f154b036075ceb319ae72ce7f2aa4bfdb3e
ThomasDHZ <dothackzero@outlook.com>
2025-05-06 13:44:25 -0300

    dll side updates.

--- VulkanEngineDLL/TextureFunc.h
+++ VulkanEngineDLL/TextureFunc.h
@@ -1,44 +1,126 @@
 #pragma once
+#include "pixel.h"
 #include "Typedef.h"
 
-extern "C"
-{
-#include "CTexture.h"
-#include "pixel.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <vulkan/vulkan_core.h>
+#include "Macro.h"
+#include "CTypedef.h"
+#include "CVulkanRenderer.h"
+
+    typedef enum ColorChannelUsed {
+        ChannelR = 1,
+        ChannelRG,
+        ChannelRGB,
+        ChannelRGBA
+    } ColorChannelUsed;
+
+    typedef enum TextureUsageEnum {
+        kUse_Undefined,
+        kUse_2DImageTexture,
+        kUse_2DDataTexture,
+        kUse_2DRenderedTexture,
+        kUse_2DDepthTexture,
+        kUse_3DImageTexture,
+        kUse_3DDataTexture,
+        kUse_3DRenderedTexture,
+        kUse_3DDepthTexture,
+        kUse_CubeMapTexture,
+        kUse_CubeMapDepthTexture
+    } TextureUsageEnum;
+
+    typedef enum TextureTypeEnum {
+        kType_UndefinedTexture,
+        kType_TextureAtlas,
+        kType_RenderedColorTexture,
+        kType_RenderedDepthTexture,
+        kType_ReadableTexture,
+        kType_DiffuseTextureMap,
+        kType_SpecularTextureMap,
+        kType_AlbedoTextureMap,
+        kType_MetallicTextureMap,
+        kType_RoughnessTextureMap,
+        kType_AmbientOcclusionTextureMap,
+        kType_NormalTextureMap,
+        kType_DepthTextureMap,
+        kType_AlphaTextureMap,
+        kType_EmissionTextureMap,
+        kType_PaletteRotationMap,
+        kType_CubeMapTexture,
+        kType_CubeMapDepthTexture,
+        kType_EnvironmentTexture,
+        kType_RenderedCubeMap,
+        kType_BakedTexture
+    } TextureTypeEnum;
+
+    DLL_EXPORT VkResult Texture_TransitionImageLayout(VkCommandBuffer commandBuffer, VkImage* image, uint32 mipmapLevels, VkImageLayout* oldLayout, VkImageLayout newLayout);
+    DLL_EXPORT VkResult Texture_CreateImage(VkDevice Device, VkPhysicalDevice physicalDevice, VkImage* image, VkDeviceMemory* memory, VkImageCreateInfo imageCreateInfo);
+    DLL_EXPORT VkResult Texture_QuickTransitionImageLayout(VkDevice device, VkCommandPool commandPool, VkQueue graphicsQueue, VkImage image, uint32 mipmapLevels, VkImageLayout* oldLayout, VkImageLayout* newLayout);
+    DLL_EXPORT VkResult Texture_CommandBufferTransitionImageLayout(VkCommandBuffer commandBuffer, VkImage image, uint32 mipmapLevels, VkImageLayout oldLayout, VkImageLayout newLayout);
+    DLL_EXPORT VkResult Texture_CopyBufferToTexture(VkDevice device, VkCommandPool commandPool, VkQueue graphicsQueue, VkImage image, VkBuffer buffer, enum TextureUsageEnum textureType, int width, int height, int depth);
+    DLL_EXPORT VkResult Texture_GenerateMipmaps(VkDevice device, VkPhysicalDevice physicalDevice, VkCommandPool commandPool, VkQueue graphicsQueue, VkImage image, VkFormat* textureByteFormat, uint32 mipmapLevels, int width, int height, bool usingMipMaps);
+    DLL_EXPORT VkResult Texture_CreateTextureView(VkDevice device, VkImageView* view, VkImage image, VkFormat format, VkImageAspectFlags imageType, uint32 mipmapLevels);
+    DLL_EXPORT VkResult Texture_CreateTextureSampler(VkDevice device, VkSamplerCreateInfo* samplerCreateInfo, VkSampler* smapler);
+    DLL_EXPORT void Texture_UpdateTextureLayout(VkDevice device, VkCommandPool commandPool, VkQueue graphicsQueue, VkImage image, VkImageLayout* oldImageLayout, VkImageLayout* newImageLayout, uint32 mipLevel);
+    DLL_EXPORT void Texture_UpdateCmdTextureLayout(VkCommandBuffer* commandBuffer, VkImage image, VkImageLayout oldImageLayout, VkImageLayout* newImageLayout, uint32 MipLevel);
+    DLL_EXPORT void Texture_UploadTexture(VkDevice device,
+        VkPhysicalDevice physicalDevice,
+        VkCommandPool commandPool,
+        VkQueue graphicsQueue,
+        int* width,
+        int* height,
+        int* depth,
+        VkFormat* textureByteFormat,
+        uint* mipmapLevels,
+        void* textureData,
+        VkImage* textureImage,
+        VkDeviceMemory* textureMemory,
+        VkImageLayout* textureImageLayout,
+        enum ColorChannelUsed* colorChannelUsed,
+        const char* filePath,
+        bool usingMipMaps);
+
+#ifdef __cplusplus
 }
+#endif
+
+#include "TextureFunc.h"
+#include "CBuffer.h"
 
 DLL_EXPORT void Texture_CreateImageTexture(VkDevice device,
-	VkPhysicalDevice physicalDevice,
-	VkCommandPool commandPool,
-	VkQueue graphicsQueue,
-	int* width,
-	int* height,
-	int* depth,
-	VkFormat textureByteFormat,
-	uint mipmapLevels,
-	VkImage* textureImage,
-	VkDeviceMemory* textureMemory,
-	VkImageLayout* textureImageLayout,
-	enum ColorChannelUsed* colorChannelUsed,
-	enum TextureUsageEnum textureUsage,
-	const Pixel& clearColor,
-	bool usingMipMap);
+    VkPhysicalDevice physicalDevice,
+    VkCommandPool commandPool,
+    VkQueue graphicsQueue,
+    int* width,
+    int* height,
+    int* depth,
+    VkFormat textureByteFormat,
+    uint mipmapLevels,
+    VkImage* textureImage,
+    VkDeviceMemory* textureMemory,
+    VkImageLayout* textureImageLayout,
+    enum ColorChannelUsed* colorChannelUsed,
+    enum TextureUsageEnum textureUsage,
+    const Pixel& clearColor,
+    bool usingMipMap);
 
 DLL_EXPORT void Texture_CreateImageTexture(VkDevice device,
-	VkPhysicalDevice physicalDevice,
-	VkCommandPool commandPool,
-	VkQueue graphicsQueue,
-	int* width,
-	int* height,
-	int* depth,
-	VkFormat textureByteFormat,
-	uint mipmapLevels,
-	VkImage* textureImage,
-	VkDeviceMemory* textureMemory,
-	VkImageLayout* textureImageLayout,
-	enum ColorChannelUsed* colorChannelUsed,
-	enum TextureUsageEnum textureUsage,
-	const String& filePath,
-	bool usingMipMap);
+    VkPhysicalDevice physicalDevice,
+    VkCommandPool commandPool,
+    VkQueue graphicsQueue,
+    int* width,
+    int* height,
+    int* depth,
+    VkFormat textureByteFormat,
+    uint mipmapLevels,
+    VkImage* textureImage,
+    VkDeviceMemory* textureMemory,
+    VkImageLayout* textureImageLayout,
+    enum ColorChannelUsed* colorChannelUsed,
+    enum TextureUsageEnum textureUsage,
+    const String& filePath,
+    bool usingMipMap);
 
-//void Texture_SaveTexture(VkDevice device, VkCommandPool commandPool, VkQueue graphicsQueue, const char* filename, SharedPtr<Texture> texture, ExportTextureFormat textureFormat, uint32 channels);
